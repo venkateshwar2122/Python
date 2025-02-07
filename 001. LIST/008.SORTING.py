987dad01512341c63194df941e45e6b73b833962
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-07 22:58:49 +0530

    Create 008.SORTING.py

---
+++ 001. LIST/008.SORTING.py
@@ -0,0 +1,50 @@
+#SORTING 
+
+1. sort()        ------> sort in first line and print in next line
+    a. ascending sort
+    b. descending sort
+
+2. sorted()      --------> sort and print in the same line
+    a. ascending sort
+    b. descending sort
+
+____________________
+
+1. sort()
+a. ascending sort
+
+l=[5,8,7,2,3,8,3,4,1]
+l.sort()                        # sorted first
+print(l)                        # then printed
+
+o/p: [1, 2, 3, 3, 4, 5, 7, 8, 8]
+
+'''
+NOTE: for above code don't sort and print in the same line:
+              l=[5,8,7,2,3,8,3,4,1]
+              print(l.sort())           #  DON'T SORT AND PRINT IN SAME LINE
+
+              o/p: none                 # NOT GETTING OUTPUT
+'''
+
+b. descending sort
+
+l=[5,8,7,2,3,8,3,4,1]
+l.sort(reverse=True)    # T is capital
+print(l)
+
+o/p: [8, 8, 7, 5, 4, 3, 3, 2, 1]
+_______________________________________________________________
+
+2.SORTED
+
+----> sort and print in the same line
+
+l=[5,8,7,2,3,8,3,4,1]
+print(sorted(l))                       # [1, 2, 3, 3, 4, 5, 7, 8, 8]
+print(sorted(l,reverse=True))          #  [8, 8, 7, 5, 4, 3, 3, 2, 1]
+print(l)                               # [5, 8, 7, 2, 3, 8, 3, 4, 1]
+
+
+
+                            

c6ea7bd7d073241cf85fd15f6a3b4a7c806d73f5
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-07 23:40:17 +0530

    Create 012.count().py

---
+++ 001. LIST/012.count().py
@@ -0,0 +1,7 @@
+# TO COUNT HOW MANY NUMBER OF TIMES A GIVEN ELEMENT WAS REPEATED IN THE LIST
+
+l=[5,8,7,2,3,8,3,4,1]
+print(l.count(3))              # 2
+print(l.count(2))              # 1
+print(l.count(9))              # 0
+

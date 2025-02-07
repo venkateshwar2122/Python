8d545b23f77f8031e609f735ad3152963a11f6a8
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-08 00:28:23 +0530

    Update 007.merge_two_sorted_list.py

--- 001. LIST/006. PYTHON PROGRAMS/007.merge_two_sorted_list.py
+++ 001. LIST/006. PYTHON PROGRAMS/007.merge_two_sorted_list.py
@@ -1,1 +1,37 @@
+QUE: merge 2 sorted into a single sorted list
 
+# NOTE THAT BOTH THE GIVEN INPUT LISTS SHOULD BE SORTED
+
+l=list(map(int,input().split()))
+m=list(map(int,input().split()))
+r=[]
+i=j=0
+
+while i<len(l) and j<len(m):
+  if l[i]<m[j]:
+    r.append(l[i])
+    i=i+1
+  else:
+    r.append(m[j])
+    j=j+1
+
+while i<len(l):             # if elements are left in l list
+  r.append(l[i])
+  i=i+1
+
+while j<len(m):              # if elements are left in m list
+  r.append(m[j])
+  j=j+1
+
+print(r)
+  
+  
+i/p:
+1 5 8 9
+0 4 7 8 9 66
+o/p:
+[0, 1, 4, 5, 7, 8, 8, 9, 9, 66]
+
+explanation:
+i pointer in l list
+j pointer in m list

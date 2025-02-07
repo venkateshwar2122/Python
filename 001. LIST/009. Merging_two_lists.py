b38bc6b8ca1450f502e78fb5980c404cecd33f32
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-07 23:16:53 +0530

    Create 009. Merging_two_lists.py

---
+++ 001. LIST/009. Merging_two_lists.py
@@ -0,0 +1,10 @@
+# MERGING TWO LISTS
+
+a) using +
+b) using extend
+
+l=[5,6,4,3]
+m=[10,20,30,60]
+print(l+m)                    # [5, 6, 4, 3, 10, 20, 30, 60]
+l.extend(m)
+print(l)                      # [5, 6, 4, 3, 10, 20, 30, 60]

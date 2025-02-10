5e4f91843334f18d6e5c875dedbf9ef483374475
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-10 11:02:19 +0530

    Update 003.examples.py

--- 001. LIST/014. SLICING/003.examples.py
+++ 001. LIST/014. SLICING/003.examples.py
@@ -5,7 +5,10 @@ print(l[5:])
 print(l[::2])
 print(l[len(l)-3 : ])                # [2, 9, 1]                    IMP
 print(l[-3:])                        # [2, 9, 1]                    IMP
-print(l[-3:-6])                      # []
+print(l[-3:-6])                      # []                           IMP
+print(l[-3:-6:-1])                   # [2, 4, 3]                    IMP
+print(l[-4:2:-1])                    # [4, 3, 6, 9, 7]               IMP
+print(l[-4:2])                       # []                            IMP
 
 
 # INDEXING IN ABOVE CODE
@@ -28,13 +31,88 @@ l[-6] refers to the 6th last element
 Slicing Rule: l[start:stop]
 
 start = -3  is after stop = -6 .
-Since slicing moves left to right by default (+1 step), it cannot move backward from -3 to -6.
-Note that, slicing only works forward unless a negative step is used.
+Since slicing moves left to right by default(+1 step) unless negative step is used,therefore  it cannot move backward from -3 to -6.
+Note that, slicing only works forward UNLESS a negative step is used.
 Hence, Python returns an empty list [], meaning "no elements found in the forward direction."
+
+MY CONCLUSION:
+formulae: moves left to right unless -ve step is used
+here start=-3 and step=+1(default). 
+Since no -ve step is used therefore it will always move from left to right.
+means index -3 then index -2 then index -1
+but here stop=-6
+therefore its impossible to move backward
 
 _______________________________________
 
 2.
+
+print(l[-3:-6:-1])                # [2, 4, 3]
+
+MY CONCLUSION:
+formulae: moves left to right unless -ve step is used
+here start=-3 , step=-1 
+since here -ve step is used therefore it can move backwards!!
+here stop =-6
+means index -3 then index -4 then index -5
+
+_______________________________________________
+
+3.
+
+print(l[:-3])                        # [5, 8, 2, 7, 9, 6, 3, 4]      IMP
+
+MY CONCLUSION:
+formulae:moves left to right unless -ve step is used
+here start=0(by default), step=+1(by default)
+since here no -ve step is used therefore it will always move left to right
+means start=0 to stop =-3
+means index0 to index -3
+
+l = [5,  8,  2,  7,  9,  6,  3,  4,  2,  9,  1]
+      0                                             (Positive Index)
+                                     -3              (Negative Index)
+
+__________________________________________________________
+
+4.
+
+print(l[-4:2:-1])                    # [4, 3, 6, 9, 7]               IMP
+
+MY CONCLUSION:
+formulae: moves left to right unless -ve step is used
+here start = -4 and step=-1
+since here -ve step is used therefore it can move backwards!!
+here stop =2
+means index -4 to  index 2
+
+l = [5,  8,  2,  7,  9,  6,  3,  4,  2,  9,  1]
+             2                                      (Positive Index)
+                                 -4                 (Negative Index)
+
+
+5. 
+
+print(l[-4:2])                       # []                            IMP
+
+MY CONCLUSION:
+formulae: moves left  to right unless -ve step is used
+here start=-4 and step=+1(by default)
+Since no -ve step is used therefore it will always move from left to right.
+
+but here stop=2
+therefore its impossible to move backward
+
+
+l = [5,  8,  2,  7,  9,  6,  3,  4,  2,  9,  1]
+                                 -4                (Positive Index)
+             2                                     (Negative Index)
+
+
+
+
+
+
 
 
 

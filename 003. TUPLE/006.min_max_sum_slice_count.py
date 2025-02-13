26ffd879bd5602c78f1120fc7065d4fa765e51c4
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-13 10:30:06 +0530

    Create 006.min_max_sum_slice_count.py

---
+++ 003. TUPLE/006.min_max_sum_slice_count.py
@@ -0,0 +1,8 @@
+#  min , max, sum, slice, count
+
+t=(6,8,23,5,7,8,3)
+print(min(t))         # 3
+print(max(t))         # 23
+print(sum(t))         # 60
+print(t[2:4])         # (23, 5)
+print(t.count(8))     # 2                         8 is repeated two times

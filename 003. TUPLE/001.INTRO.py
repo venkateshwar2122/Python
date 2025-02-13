8222913d994ab410fe0dd1acc3e63ed3fdba9a7b
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-13 10:01:33 +0530

    Create 001.INTRO.py

---
+++ 003. TUPLE/001.INTRO.py
@@ -0,0 +1,8 @@
+1. Tuple is collection of elemnts
+2. syntax:     (ele1,ele2,ele3,...)
+3.Tuple is immutable, means once a tuple object is created then u cannot change or edit it ! means u cannot do insertion,deletion,updation,sorting etc. 
+NOTE: wheras LIST is mutable.
+4. support +ve,-ve indxing
+5. can do slicing
+
+NOTE: LIST,SET,DICTIONARY ARE MUTABLE other than this any other datatype(including tuple) are immutable !!

645d77d5eb6dafa687017310ad436bf7b3fc81dd
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-13 10:25:15 +0530

    Create 005.sorting_exception.py

---
+++ 003. TUPLE/005.sorting_exception.py
@@ -0,0 +1,9 @@
+
+# NOTE: tuple is immutable, therfore it cannot be sorted , but nontheless if we try to sort tuple,the output will be list
+
+t=(6,8,23,5,7,8,3)
+print(sorted(t))
+
+o/p:
+
+[3, 5, 6, 7, 8, 8, 23]    OUTPUT IS A LIST

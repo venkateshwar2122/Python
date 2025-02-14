8566d07bc503b757d2c1bdfe9eef3edfe05af3a0
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-14 11:09:27 +0530

    Create 001.intro.py

---
+++ 005. DICTIONARY/001.intro.py
@@ -0,0 +1,11 @@
+1. Dictionary is collection of key-value pair.
+
+2. In Python, dictionary keys must be unique and hashable. This means the key must be of an immutable data type. Some common 
+immutable data types that can be used as dictionary keys are:
+Strings (str)
+Numbers (int, float, complex)
+Tuples (only if all the elements inside the tuple are immutable)
+Booleans (True, False)
+
+
+3.  dictionaries in Python are mutable! 🎯 This means you can add, remove, or modify key-value pairs after the dictionary is created.

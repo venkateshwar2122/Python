777e1ec1a81d06d1fd823ef359e7006fa76c9a81
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-15 09:18:34 +0530

    Update 002.py

--- 001. LIST/006. PYTHON PROGRAMS/013. SORTING ALGORITHMS/002. SELECTION SORT/002.py
+++ 001. LIST/006. PYTHON PROGRAMS/013. SORTING ALGORITHMS/002. SELECTION SORT/002.py
@@ -1,6 +1,14 @@
 Selection Sort is a comparison-based sorting algorithm that repeatedly finds the smallest element("k") from 
 the unsorted part of the list and swaps it with the first element("i") of that part.
 
+
+  in bubble sort, hume jahan bada element milta tha use swap kar kar ke last index tak pahuchate they.
+means in bubble sort there was always swapping, but whereas in selection sort hum sabse chote element ko variable "k" mai store karte hai
+then hum uss k variable ko last index tak le jaakar "i" pointer(jo first index ko point kar raha hai) se swap kar dete hai, therefore in selection sort 1 baar mai 1 hi swap hota hai !
+
+
+
+
 l = list(map(int, input().split()))
 for i in range(len(l)-1):
   k=i

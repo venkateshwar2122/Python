e188c7137b1a36614c32cd68cc6356f857fe88cf
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-15 15:45:57 +0530

    Create 006.searching.py

---
+++ 004. SET/006.searching.py
@@ -0,0 +1,8 @@
+NOTE: since set doen't main indexing therfore u cannot find index of the seached element.
+you can use membership operator to know that a given element is present in set or not.
+
+s={4,8,7,100,4,3,2,4,4}
+print(100 in s)
+
+o/p:
+TRUE

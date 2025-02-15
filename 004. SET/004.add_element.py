4a5ebaa0e80eff680c5aca587fdab7f80fb7bf2d
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-15 15:38:23 +0530

    Create 004.add_element.py

---
+++ 004. SET/004.add_element.py
@@ -0,0 +1,10 @@
+NOTE: since set doen't maintain indexing and insertion order therfore when u are adding elemenrt in set then it can be positioned
+in random position
+
+s={4,8,7,1,4,3,2,4,4}
+
+s.add(99)
+print(s)
+
+o/p:
+{1, 2, 3, 4, 99, 7, 8}

180f3e877c5bed56f1d410335750939261cec1fb
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-15 15:31:10 +0530

    Update and rename 001.py to 001.intro.py

---
+++ 004. SET/001.intro.py
@@ -0,0 +1,9 @@
+SET is colection of elements
+
+Properties of Sets:-
+No Duplicates → Duplicate values are automatically removed.
+Unordered →     Elements do not have a fixed position means doesn't maintain inserion order thus it doesn't support indexing
+Mutable (set is mutable) → You can add or remove elements.
+Cannot Contain Mutable Data Types(set elements are not immutable) → Lists & dictionaries cannot be set elements, but tuples can.
+
+NOTE: List, set,dictionary are mutable but tuple is immutable

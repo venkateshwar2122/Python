26566a7c1cf915400e7e8468bfd39b432f80a4f2
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-15 15:21:29 +0530

    Create 009.Nested_dictionary.py

---
+++ 005. DICTIONARY/009.Nested_dictionary.py
@@ -0,0 +1,10 @@
+in below dictionary u can see that key's 101 value is another dictionary.
+
+d={101: {"name":"abc", "age": 20, "dept":"cse"},
+   102: {"name":"xyz", "age":22, "dept":"ece"}
+   }
+
+print(d)
+
+o/p:
+{101: {'name': 'abc', 'age': 20, 'dept': 'cse'}, 102: {'name': 'xyz', 'age': 22, 'dept': 'ece'}}

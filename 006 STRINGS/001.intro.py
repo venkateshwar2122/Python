b83296feccfa96dc78e3c7afb26a29126cadb605
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-18 10:13:55 +0530

    Create 001.intro.py

---
+++ 006 STRINGS/001.intro.py
@@ -0,0 +1,13 @@
+# String Data Type in Python
+A string (str) in Python is a sequence of characters enclosed in single ('), double (") or triple quotes (''' """). 
+Strings are immutable, meaning they cannot be changed after creation.
+
+s="hello"
+print(s)
+type(s)
+
+o/p:
+hello
+str
+
+

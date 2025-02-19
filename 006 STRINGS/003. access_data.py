eede034a214f059101af64013434b12f886463f5
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-02-19 10:03:30 +0530

    Create 003. access_data.py

---
+++ 006 STRINGS/003. access_data.py
@@ -0,0 +1,27 @@
+#ACCESS DATA FROM STRING
+
+1. ACCESS EACH ELEMENT
+
+s = "abcd"
+for ch in s:
+  print(ch)
+
+O/P:
+a
+b
+c
+d
+____________________________________
+
+2. ACCESS THROUGH INDEXING
+
+s = "abcd"
+for i in range(len(s)):
+  print(s[i])
+
+
+o/p:
+a
+b
+c
+d

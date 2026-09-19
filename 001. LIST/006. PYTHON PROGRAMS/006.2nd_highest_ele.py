680710072ccc8046d259be363b518ffe41f81378
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2026-09-19 20:11:15 +0530

    Update 006.2nd_highest_ele.py

--- 001. LIST/006. PYTHON PROGRAMS/006.2nd_highest_ele.py
+++ 001. LIST/006. PYTHON PROGRAMS/006.2nd_highest_ele.py
@@ -9,3 +9,22 @@ if len(unique_l) > 1:
     print(unique_l[1])  # Second largest element
 else:
     print("No second largest element")  # Edge case: All elements are the same
+
+
+
+MINE METHOD-----------______________________________________________________
+a = [860, 900, 800, 700, 850, 100, 200, 300, 400, 500]
+
+max1 = a[0]
+max2 = -1
+
+for i in range(len(a)):
+
+    if a[i] > max1:
+        max2 = max1
+        max1 = a[i]
+
+    elif a[i] > max2 and a[i] != max1:
+        max2 = a[i]
+
+print(max2)
